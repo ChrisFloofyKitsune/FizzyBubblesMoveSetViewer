b9d76fee451ca74ae4ce7be88f591a8c886c28eb
ChrisClark13 <krizasan@gmail.com>
2020-08-02 06:36:07 -0600

    Fixening 2 BOOGALOO

--- js/moveListViewer.js
+++ js/moveListViewer.js
@@ -1,44 +1,42 @@
 var pokemonData = null;
 var pokemon = null;
 
-$(document).ready(function()
-{
-    $.getJSON("js/pokemonMoveList.json", function(data) {
+$(document).ready(function () {
+    $.getJSON("js/pokemonMoveList.json", function (data) {
         pokemonData = data;
 
         var pokemonSelect = $("#pokemonSelect");
         pokemonData.Pokemon.forEach(pokemon => {
-           pokemonSelect.append(new Option(pokemon.Name + " #" + pokemon.DexNum, pokemon.DexNum)); 
+            pokemonSelect.append(new Option(pokemon.Name + " #" + pokemon.DexNum, pokemon.DexNum));
         });
         pokemonSelect.selectpicker("refresh");
         pokemonSelect.change(OnSelectPokemon);
-        $("#pokemonFormSelect").change(OnSelectForm);
+        $("#pokemonFormSelect").change(RefreshTables);
     });
 });
 
-function OnSelectPokemon()
-{
+function OnSelectPokemon() {
     pokemon = pokemonData.Pokemon.find(pokemon => pokemon.DexNum == this.value);
 
     var formSelect = $("#pokemonFormSelect");
     formSelect.empty();
     formSelect.append(new Option(pokemon.DefaultForm, pokemon.DefaultForm));
-    if (pokemon.AltForms.length == 0)
-    {
+    if (pokemon.AltForms.length == 0) {
         formSelect.prop("disabled", true);
     }
-    else
-    {
+    else {
         pokemon.AltForms.forEach(form => {
             formSelect.append(new Option(form, form));
         })
         formSelect.prop("disabled", false)
     }
     formSelect.selectpicker("refresh");
+    formSelect.val(pokemon.DefaultForm);
+
+    RefreshTables();
 }
 
-function OnSelectForm()
-{
+function RefreshTables() {
     var form = this.value;
 
     var levelUpMoveTable = $("#levelUpMoveTable");
@@ -51,23 +49,24 @@ function OnSelectForm()
     tutorMoveTable.empty();
     machineMoveTable.empty;
 
-    if (pokemon != null && (pokemon.DefaultForm = form || pokemon.AltForms.includes(form)))
-    {
+    if (pokemon != null && (pokemon.DefaultForm = form || pokemon.AltForms.includes(form))) {
         pokemon.LevelUpMoveLists.find(list => list.Form == form)
             .LevelUpMoves.forEach(move => {
                 levelUpMoveTable.append("<tr><td>" + move.Name + "</td><td>" + move.Level + "</td></tr>");
             });
 
-        pokemon.EggMoves.filter(m => m.Forms.includes(form)).forEach(m => {
-            eggMoveTable.append("<tr><td>" + m.Name + "</td></tr>")
-        });
+        if (pokemon.EggMoves != null)
+            pokemon.EggMoves.filter(m => m.Forms.includes(form)).forEach(m => {
+                eggMoveTable.append("<tr><td>" + m.Name + "</td></tr>")
+            });
 
-        pokemon.TutorMoves.filter(m => m.Forms.includes(form)).forEach(m => {
-            eggMoveTable.append("<tr><td>" + m.Name + "</td></tr>")
-        });
-
-        pokemon.MachineMoves.filter(m => m.Forms.includes(form)).forEach(m => {
-            eggMoveTable.append("<tr><td>" + m.Name + "</td></tr>")
-        });
+        if (pokemon.TutorMoves != null)
+            pokemon.TutorMoves.filter(m => m.Forms.includes(form)).forEach(m => {
+                tutorMoveTable.append("<tr><td>" + m.Name + "</td></tr>")
+            });
+        if (pokemon.MachineMoves != null)
+            pokemon.MachineMoves.filter(m => m.Forms.includes(form)).forEach(m => {
+                machineMoveTable.append("<tr><td>" + m.Name + "</td></tr>")
+            });
     }
 }
