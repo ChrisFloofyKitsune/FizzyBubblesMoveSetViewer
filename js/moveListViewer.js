63cd93002adf8a1ae5635a89d0505db469917337
ChrisClark13 <krizasan@gmail.com>
2020-08-02 04:54:45 -0600

    attempt to load pokemon data into select

--- js/moveListViewer.js
+++ js/moveListViewer.js
@@ -2,8 +2,12 @@ var pokemonData;
 
 $(document).ready(function()
 {
-
     $.getJSON("js/pokemonMoveList.json", function(data) {
         pokemonData = data;
+
+        var pokemonSelect = $("#pokemonSelect");
+        pokemonData.array.forEach(pokemon => {
+           pokemonSelect.append(new Option(pokemon.Name, pokemon.DexNum)); 
+        });
     });
 });
