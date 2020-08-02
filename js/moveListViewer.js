9e15660fc0ad3cafd68cbd33c856d8f963dc3551
ChrisClark13 <krizasan@gmail.com>
2020-08-02 06:11:17 -0600

    this should be it!

--- js/moveListViewer.js
+++ js/moveListViewer.js
@@ -1,4 +1,5 @@
-var pokemonData;
+var pokemonData = null;
+var pokemon = null;
 
 $(document).ready(function()
 {
@@ -11,12 +12,13 @@ $(document).ready(function()
         });
         pokemonSelect.selectpicker("refresh");
         pokemonSelect.change(OnSelectPokemon);
+        $("#pokemonFormSelect").change(OnSelectForm);
     });
 });
 
 function OnSelectPokemon()
 {
-    var pokemon = pokemonData.Pokemon.find(pokemon => pokemon.DexNum == this.value);
+    pokemon = pokemonData.Pokemon.find(pokemon => pokemon.DexNum == this.value);
 
     var formSelect = $("#pokemonFormSelect");
     formSelect.empty();
@@ -33,4 +35,39 @@ function OnSelectPokemon()
         formSelect.prop("disabled", false)
     }
     formSelect.selectpicker("refresh");
+}
+
+function OnSelectForm()
+{
+    var form = this.value;
+
+    var levelUpMoveTable = $("#levelUpMoveTable");
+    var eggMoveTable = $("#eggMoveTable");
+    var tutorMoveTable = $("#tutorMoveTable");
+    var machineMoveTable = $("#machineMoveTable");
+
+    levelUpMoveTable.empty();
+    eggMoveTable.empty();
+    tutorMoveTable.empty();
+    machineMoveTable.empty;
+
+    if (pokemon != null && pokemon.AltForms.includes(form))
+    {
+        pokemon.LevelUpMoveLists.find(list => list.Form == form)
+            .LevelUpMoves.forEach(move => {
+                levelUpMoveTable.append("<tr><td>" + move.Name + "</td><td>" + move.Level + "</td></tr>");
+            });
+
+        pokemon.EggMoves.filter(m => m.Forms.includes(form)).forEach(m => {
+            eggMoveTable.append("<tr><td>" + m.Name + "</td></tr>")
+        });
+
+        pokemon.TutorMoves.filter(m => m.Forms.includes(form)).forEach(m => {
+            eggMoveTable.append("<tr><td>" + m.Name + "</td></tr>")
+        });
+
+        pokemon.MachineMoves.filter(m => m.Forms.includes(form)).forEach(m => {
+            eggMoveTable.append("<tr><td>" + m.Name + "</td></tr>")
+        });
+    }
 }
