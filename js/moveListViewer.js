a5a7d332fad22c0bdd4dc4aa75c785903990d2cd
ChrisClark13 <krizasan@gmail.com>
2020-08-05 23:29:16 -0600

    abilityDex, URL Hash link

--- js/moveListViewer.js
+++ js/moveListViewer.js
@@ -1,7 +1,10 @@
 var pokemonList = null;
-var pokemon = null;
 var moveDex = null;
+var abilityDex = null;
+
+var pokemon = null;
 var currentForm = null;
+
 var expanded = false;
 var superExpanded = false;
 
@@ -10,15 +13,22 @@ $(document).ready(function () {
     console.log("Loading Pokemon List");
     $.getJSON("js/pokemonMoveList.json", function (data) {
         console.log("Loaded Pokemon List");
+
         pokemonList = data.Pokemon;
         pokemon = pokemonList[0];
+        currentForm = pokemon.DefaultForm;
+
+        LoadURLHash();
 
         var pokemonSelect = $("#pokemonSelect");
         pokemonList.forEach(p => {
             pokemonSelect.append(new Option(p.Name + " #" + p.DexNum, p.DexNum));
         });
         pokemonSelect.selectpicker("refresh");
+        pokemonSelect.val(pokemon.DexNum);
         pokemonSelect.change(OnSelectPokemon);
+        pokemonSelect.selectpicker("refresh");
+
         $("#pokemonFormSelect").change(OnSelectForm);
 
         $("#expandTableCheckbox").change(function () {
@@ -32,15 +42,28 @@ $(document).ready(function () {
             RefreshTables();
         });
 
+        window.onhashchange = function() {
+            LoadURLHash();
+            pokemonSelect.val(pokemon.DexNum);
+            pokemonSelect.selectpicker("refresh");
+            UpdateFormSelect(currentForm);
+            RefreshTables(currentForm);
+        }
 
-
-        RefreshTables(pokemon.DefaultForm);
+        UpdateFormSelect(currentForm);
+        RefreshTables(currentForm);
     });
 
     console.log("Loading Move Dex");
     $.getJSON("js/moveDex.json", function (data) {
         console.log("Loaded Move Dex");
         moveDex = data;
+    });
+
+    console.log("Loading Ability Dex");
+    $.getJSON("js/abilityDex.json", function (data) {
+        console.log("Loaded Ability Dex");
+        abilityDex = data;
     });
 });
 
@@ -121,6 +144,24 @@ const specialCaseHumpies = [
         result: "029m"
     }
 ];
+
+function LoadURLHash() {
+    if (document.location.hash) {
+        var match = document.location.hash.match(/DexNum=(.*?)(?:&|$)/);
+        if (match != null) {
+            //console.log(match);
+            var num = parseInt(match[1]);
+            if (num != NaN) {
+                pokemon = pokemonList[num - 1] ?? pokemon;
+
+                match = document.location.hash.match(/Form=(.*?)(?:&|$)/);
+                var form = (match == null) ? null : match[1];
+                if (form != null && (pokemon.DefaultForm == form || pokemon.AltForms.includes(form)))
+                    currentForm = form;
+            }
+        }
+    }
+}
 
 function FetchHumpyLink() {
     if (pokemon == null)
@@ -214,6 +255,12 @@ const superExpandedHeader =
 function OnSelectPokemon() {
     pokemon = pokemonList.find(p => p.DexNum == this.value);
 
+    UpdateFormSelect(pokemon.DefaultForm);
+
+    RefreshTables(pokemon.DefaultForm);
+}
+
+function UpdateFormSelect(selectedForm) {
     var formSelect = $("#pokemonFormSelect");
     formSelect.empty();
     formSelect.append(new Option(pokemon.DefaultForm, pokemon.DefaultForm));
@@ -223,15 +270,13 @@ function OnSelectPokemon() {
     else {
         pokemon.AltForms.forEach(form => {
             formSelect.append(new Option(form, form));
-        })
-        formSelect.prop("disabled", false)
+        });
+        formSelect.prop("disabled", false);
     }
     formSelect.selectpicker("refresh");
 
-    formSelect.val(pokemon.DefaultForm);
+    formSelect.val(selectedForm);
     formSelect.selectpicker("refresh");
-
-    RefreshTables(pokemon.DefaultForm);
 }
 
 function OnSelectForm() {
@@ -240,8 +285,13 @@ function OnSelectForm() {
 
 function RefreshTables(form = null) {
 
+    if (pokemon == null)
+        return;
+
     if (form != null)
         currentForm = form;
+
+    document.location.hash = (`Name=${pokemon.Name}&DexNum=${pokemon.DexNum}` + ((currentForm != "Normal") ? `&Form=${currentForm}` : ""));
 
     if (!expanded) {
         $("#levelUpMoveHeader").html(normalLevelUpHeader);
@@ -262,11 +312,13 @@ function RefreshTables(form = null) {
         $("#machineMoveHeader").html(expandedHeader);
     }
 
+    var abilityTable = $("#abilityTable");
     var levelUpMoveTable = $("#levelUpMoveTable");
     var eggMoveTable = $("#eggMoveTable");
     var tutorMoveTable = $("#tutorMoveTable");
     var machineMoveTable = $("#machineMoveTable");
 
+    abilityTable.empty();
     levelUpMoveTable.empty();
     eggMoveTable.empty();
     tutorMoveTable.empty();
@@ -275,16 +327,36 @@ function RefreshTables(form = null) {
     $("#pokemonName").text(pokemon.Name);
     FetchHumpyLink();
 
-    if (pokemon != null && (pokemon.DefaultForm == currentForm || pokemon.AltForms.includes(currentForm))) {
-        LoadTableInformation(levelUpMoveTable, pokemon.LevelUpMoveLists.find(list => list.Form == currentForm).LevelUpMoves, true);
-        LoadTableInformation(eggMoveTable, pokemon.EggMoves.filter(m => m.Forms.includes(currentForm)));
-        LoadTableInformation(tutorMoveTable, pokemon.TutorMoves.filter(m => m.Forms.includes(currentForm)));
-        LoadTableInformation(machineMoveTable, pokemon.MachineMoves.filter(m => m.Forms.includes(currentForm)));
+    {
+        let abilities = abilityDex.filter(a => a.Pokemon.some(p => p.DexNum == pokemon.DexNum));
+        if (abilities.some(a => a.Pokemon.some(p => p.DexNum == pokemon.DexNum && p.Form != undefined))) {
+            abilities = abilities.filter(a => a.Pokemon
+                .some(p => p.DexNum == pokemon.DexNum && ((p.Form == undefined && currentForm == pokemon.DefaultForm) || p.Form == currentForm)));
+        }
+
+        abilities.forEach(a => {
+            var newHTML =
+                `<tr>
+                <td>${a.Name}</td>
+                <td>${a.GameText}</td>
+                <td>${a.EffectDetail ?? ""}</td>
+                <td><a href="${a.Link}" target="_blank"><i class="fas fa-external-link-alt"></i></a></td>
+            </tr>`
+            abilityTable.append(newHTML);
+        });
+    }
+
+
+    if (pokemon.DefaultForm == currentForm || pokemon.AltForms.includes(currentForm)) {
+        CreateMoveTable(levelUpMoveTable, pokemon.LevelUpMoveLists.find(list => list.Form == currentForm).LevelUpMoves, true);
+        CreateMoveTable(eggMoveTable, pokemon.EggMoves.filter(m => m.Forms.includes(currentForm)));
+        CreateMoveTable(tutorMoveTable, pokemon.TutorMoves.filter(m => m.Forms.includes(currentForm)));
+        CreateMoveTable(machineMoveTable, pokemon.MachineMoves.filter(m => m.Forms.includes(currentForm)));
     }
 }
 
-function LoadTableInformation(table, moves, isLevelUpTable = false) {
-    if (!expanded) {
+function CreateMoveTable(table, moves, isLevelUpTable = false) {
+    if (!expanded || moveDex == null) {
         if (isLevelUpTable) {
             moves.forEach(move => {
                 table.append(`<tr><td>${move.Name}</td><td>${move.Level}</td></tr>`);
@@ -323,7 +395,7 @@ function LoadTableInformation(table, moves, isLevelUpTable = false) {
             if (superExpanded)
                 newHTML += `<td>${moveInfo.CriticalHitRate}</td>`;
             newHTML += `<td>${moveInfo.MakesPhysicalContact}</td>`;
-            newHTML += `<td><a href="${moveInfo.Link}"><i class="fas fa-external-link-alt"></i></a></td>`;
+            newHTML += `<td><a href="${moveInfo.Link}" target="_blank"><i class="fas fa-external-link-alt"></i></a></td>`;
 
             newHTML += "</tr>";
             table.append(newHTML);
